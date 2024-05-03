2577011c9349a01d74f216a9e7a1c661db17f1a7
Milan Sony <milansonyofficial@gmail.com>
2024-05-03 22:56:14 +0530

    Feat: Base URl(/) created

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const app = express();
 // env
 require('dotenv').config();
 
+
 // DB
 const db = require('./config/db');
 db.connect();
+
+
+// Base URL (/)
+const routes = require('./routes')
+app.use('/', routes);
+
 
 app.listen((process.env.PORT || 5000), () => {
     console.log(`\nServer listening on port: ${process.env.PORT || 5000}`)
