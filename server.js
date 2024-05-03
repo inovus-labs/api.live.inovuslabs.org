798079110eac7239a9d285fe244f7ae0ea82f599
Milan Sony <milansonyofficial@gmail.com>
2024-05-03 23:25:42 +0530

    '404' and '500' error status message added

--- server.js
+++ server.js
@@ -16,6 +16,22 @@ const routes = require('./routes')
 app.use('/', routes);
 
 
+app.use((req, res, next) => {
+    res.status(404).json({
+        status: 404,
+        message: 'Resource not found'
+    })
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({
+        status: 500,
+        message: 'Internal server error'
+    })
+});
+
+
 app.listen((process.env.PORT || 5000), () => {
     console.log(`\nServer listening on port: ${process.env.PORT || 5000}`)
 });
