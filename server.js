8566755c4141f15df59023556a207d943a687a05
Milan Sony <milansonyofficial@gmail.com>
2024-05-03 22:51:22 +0530

    MongoDB successfully connected

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 // env
 require('dotenv').config();
 
+// DB
+const db = require('./config/db');
+db.connect();
 
 app.listen((process.env.PORT || 5000), () => {
     console.log(`\nServer listening on port: ${process.env.PORT || 5000}`)
